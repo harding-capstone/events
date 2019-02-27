663a4061b8e861a6beaa2a702badeb9d14b7c2e8
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-02-27 15:26:44 -0600

    Don't export empty modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.shepherdjerred.capstone.events {
   requires static lombok;
   requires com.shepherdjerred.capstone.logic;
   requires com.shepherdjerred.capstone.common;
-  exports com.shepherdjerred.capstone.events.chat;
-  exports com.shepherdjerred.capstone.events.connection;
-  exports com.shepherdjerred.capstone.events;
+//  exports com.shepherdjerred.capstone.events.chat;
+//  exports com.shepherdjerred.capstone.events.connection;
+//  exports com.shepherdjerred.capstone.events;
 }
